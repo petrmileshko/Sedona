97009d5e69176ff9fb48ae62a10530ef944428de
Peter <mileshko.petr@yandex.ru>
2023-01-18 01:38:00 +0300

    Update modal.js

--- modal.js
+++ modal.js
@@ -2,12 +2,12 @@ let search = document.querySelector('.search');
 let modal = document.querySelector('.modal-container');
 let closeButton = document.querySelector('.modal-close-button');
 
-search.addEventListener('click', function(event) {
+search.addEventListener('click', function(event) { // Обработчик кнопки Поиск гостиниц
 	event.preventDefault();
 	modal.classList.remove('modal-off');
 });
 
-closeButton.addEventListener('click', function(event) {
+closeButton.addEventListener('click', function(event) { // Обработчик кнопки закрытия модального окна
 	event.preventDefault();
 	modal.classList.add('modal-off');
 })
